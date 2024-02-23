6e5d54a6619c484007b84799b497619ead313c2f
taldaitz <48563569+taldaitz@users.noreply.github.com>
2024-02-23 13:27:04 +0100

    Create users.sql

---
+++ users.sql
@@ -0,0 +1,120 @@
+INSERT INTO `user` (`lastname`,`firstname`,`phone_number`,`email`,`subscription_type`)
+VALUES
+  ("Guy","Marvin","05 20 46 11 34","egestas@yahoo.org","X9J 3I1"),
+  ("Finch","Zephania","01 79 72 44 67","cursus.in.hendrerit@outlook.org","L0K 3E0"),
+  ("Gentry","Conan","09 25 10 09 21","arcu.nunc@aol.couk","N3W 4Y4"),
+  ("Mcknight","Rylee","04 55 97 85 30","tristique.senectus.et@outlook.couk","I2Q 4C5"),
+  ("Valdez","Tanya","06 96 15 84 75","libero.mauris@aol.couk","D7Q 3G5"),
+  ("Small","Derek","08 78 76 91 24","sem@outlook.org","B6H 4J6"),
+  ("Peterson","Talon","05 33 37 75 87","in.condimentum@outlook.ca","O3N 7C8"),
+  ("Hyde","Alec","03 73 25 07 62","mauris.ut@icloud.ca","X9U 3G4"),
+  ("Sullivan","Armando","06 43 92 24 83","sed.pede@protonmail.net","A3F 2B7"),
+  ("Kaufman","Dai","08 86 48 36 37","in.consequat@aol.org","F8P 4J6");
+INSERT INTO `user` (`lastname`,`firstname`,`phone_number`,`email`,`subscription_type`)
+VALUES
+  ("Giles","Joan","08 81 38 60 82","velit.eu@aol.org","E0F 1S6"),
+  ("Velazquez","Chancellor","07 68 75 62 27","aliquam@aol.edu","F5I 4H8"),
+  ("Livingston","Hunter","01 71 80 71 64","auctor.nunc@google.edu","I3A 3W3"),
+  ("Adams","Oliver","01 74 25 66 47","facilisis.vitae.orci@aol.com","K9O 0E7"),
+  ("Fitzpatrick","Alika","03 83 38 05 28","egestas.sed.pharetra@icloud.couk","L2F 3X8"),
+  ("Chambers","Vaughan","07 23 69 94 05","ridiculus.mus@yahoo.ca","W8K 5H6"),
+  ("Estes","Martin","07 42 28 34 14","primis.in@outlook.com","N5M 7D1"),
+  ("Terry","Cade","03 19 43 60 72","neque.sed.dictum@outlook.edu","E6L 8R6"),
+  ("Carpenter","Ross","01 67 44 66 00","nonummy.ac.feugiat@icloud.couk","W3K 1H9"),
+  ("Tucker","Timothy","08 26 69 58 68","maecenas@protonmail.org","F5V 1G8");
+INSERT INTO `user` (`lastname`,`firstname`,`phone_number`,`email`,`subscription_type`)
+VALUES
+  ("Hood","Summer","09 63 24 83 12","suspendisse.dui@yahoo.org","J7W 8Z8"),
+  ("Howell","Abraham","08 43 51 60 68","posuere@protonmail.edu","F6B 5G3"),
+  ("Stout","Aphrodite","06 55 46 51 69","est@aol.com","U7W 4L0"),
+  ("Case","Zahir","08 27 36 88 48","mauris.morbi.non@hotmail.couk","I5R 9X3"),
+  ("David","Meghan","04 62 42 66 87","praesent.eu.dui@google.couk","J8Z 5W1"),
+  ("Riley","Germane","03 79 36 55 95","cubilia.curae@yahoo.couk","Z4G 1E8"),
+  ("Finley","Oscar","02 34 73 85 10","eget.nisi@outlook.net","I7V 5E7"),
+  ("Clark","Jessica","09 54 83 51 68","ante.lectus.convallis@icloud.edu","Q2F 2X1"),
+  ("Carlson","Craig","05 29 81 31 05","convallis@aol.couk","P1I 5K5"),
+  ("Hopkins","Bree","08 72 76 77 84","nec@aol.ca","T8J 8R0");
+INSERT INTO `user` (`lastname`,`firstname`,`phone_number`,`email`,`subscription_type`)
+VALUES
+  ("Bishop","Dorothy","02 71 20 78 06","arcu.et@yahoo.ca","O7W 8X2"),
+  ("Velasquez","Rhoda","07 71 62 45 87","ultricies.adipiscing@google.couk","M2M 3O4"),
+  ("Lowe","Griffith","07 38 67 36 68","id.mollis@yahoo.couk","L2U 8E8"),
+  ("Maynard","Rhona","05 58 78 54 80","ante.lectus@outlook.com","O5D 4D2"),
+  ("Sloan","Lilah","05 74 48 14 41","quam.vel@google.com","E8P 4D9"),
+  ("Abbott","Helen","04 63 25 80 42","vulputate@outlook.net","Q5J 8A1"),
+  ("Frank","Hall","05 12 12 63 36","in@aol.couk","E7G 8X7"),
+  ("Higgins","Madonna","01 76 65 32 53","cursus@icloud.net","A4W 8Y5"),
+  ("Cervantes","Lani","03 58 03 22 18","at.lacus.quisque@aol.net","D2J 9M2"),
+  ("Torres","Lee","04 22 56 77 63","mauris.nulla@hotmail.edu","M3W 5X3");
+INSERT INTO `user` (`lastname`,`firstname`,`phone_number`,`email`,`subscription_type`)
+VALUES
+  ("Moody","Kylie","07 07 21 68 40","nunc.lectus@aol.net","X2N 2G3"),
+  ("Kirk","Kiara","06 58 53 34 14","nam@outlook.net","U7R 8V5"),
+  ("Decker","Rooney","04 23 18 61 76","in.molestie@protonmail.com","Y5P 2C0"),
+  ("Murphy","Melvin","04 97 49 79 85","fusce.mi@google.org","U3B 4U9"),
+  ("Gillespie","Rylee","04 36 04 26 70","risus.quisque.libero@yahoo.org","P4I 1T4"),
+  ("Mcclure","Madeline","02 97 62 10 07","pellentesque@aol.com","H8D 3W5"),
+  ("Holder","Hector","03 25 59 74 88","ac.libero.nec@outlook.ca","Z5Q 9U2"),
+  ("Tanner","Fulton","06 68 43 49 07","facilisis.non@google.com","Q9R 2O7"),
+  ("Meyer","Sebastian","06 88 23 81 34","hendrerit@yahoo.com","K7L 8G7"),
+  ("Blake","Beatrice","07 77 35 33 32","cursus.a.enim@outlook.edu","B6Q 5E8");
+INSERT INTO `user` (`lastname`,`firstname`,`phone_number`,`email`,`subscription_type`)
+VALUES
+  ("Love","Dorothy","03 27 93 98 46","ridiculus.mus.aenean@protonmail.ca","S3U 5C6"),
+  ("Ayala","Shannon","02 52 38 96 16","enim.mi@aol.couk","M9Q 5S4"),
+  ("Cook","George","02 46 31 19 19","convallis.erat@hotmail.org","H1O 9D5"),
+  ("Sharpe","Adara","06 93 40 87 22","suspendisse.commodo@aol.com","X0V 7F7"),
+  ("Browning","Flynn","02 57 12 10 26","phasellus.fermentum@aol.net","M2Y 3S7"),
+  ("Cooley","Savannah","03 75 51 82 88","luctus@aol.ca","I7O 6B6"),
+  ("Reynolds","Aurelia","08 23 56 18 22","ac@yahoo.couk","B7J 8G8"),
+  ("Ochoa","Jillian","09 74 16 76 19","erat.in@protonmail.com","B8X 6J6"),
+  ("Melton","Francesca","02 68 33 44 88","porttitor.scelerisque.neque@aol.edu","G8Q 3J7"),
+  ("Lowe","Shafira","08 08 84 85 52","eu.lacus@yahoo.couk","D7A 3R3");
+INSERT INTO `user` (`lastname`,`firstname`,`phone_number`,`email`,`subscription_type`)
+VALUES
+  ("Callahan","Pamela","02 86 73 04 39","purus.duis.elementum@hotmail.com","C1C 8J6"),
+  ("Beasley","Colton","03 21 11 10 24","semper.auctor@aol.org","D7K 6M5"),
+  ("Barrera","Robin","06 35 31 29 18","purus@icloud.net","V6P 5O5"),
+  ("Benton","Barrett","08 32 31 59 38","metus.in@outlook.com","L7I 8J2"),
+  ("Knight","Cleo","09 13 61 21 71","fringilla.mi@hotmail.com","Q5V 7L7"),
+  ("Michael","Emery","02 11 36 75 89","id.erat@outlook.com","O0F 0T7"),
+  ("Meyer","Bradley","03 56 07 15 95","ac.metus@icloud.edu","E1P 6P1"),
+  ("Sharpe","Brittany","02 65 62 01 14","amet.orci@yahoo.net","W1W 2O7"),
+  ("Rivera","Emery","01 26 72 19 88","felis.eget@hotmail.org","O7O 5N2"),
+  ("Powers","Brian","02 45 95 12 44","sed.sapien.nunc@protonmail.ca","I4F 9O8");
+INSERT INTO `user` (`lastname`,`firstname`,`phone_number`,`email`,`subscription_type`)
+VALUES
+  ("Reeves","Anne","04 47 77 86 46","elementum.purus@hotmail.org","Q2T 6L8"),
+  ("Hurley","Sophia","03 85 65 68 34","ullamcorper.duis.cursus@google.ca","T1Q 4M7"),
+  ("Manning","Sophia","06 34 66 89 45","sagittis.nullam.vitae@icloud.net","N6Q 3J2"),
+  ("Adams","Leroy","04 15 42 51 65","pellentesque.ut@aol.org","Q9G 0D5"),
+  ("Orr","Ocean","05 92 15 66 95","ligula@google.com","C0P 4X1"),
+  ("Whitney","Suki","08 14 46 22 22","nulla.tempor.augue@outlook.ca","Q8W 5B1"),
+  ("Mccarty","Megan","09 08 63 81 03","iaculis.lacus@outlook.net","S8L 9D1"),
+  ("Phillips","Keegan","05 86 08 45 97","vivamus.non.lorem@hotmail.edu","L3Y 6S6"),
+  ("Salazar","Constance","04 85 35 04 37","montes@outlook.couk","T4F 1R6"),
+  ("Pope","Acton","05 66 57 75 43","dolor.vitae.dolor@outlook.com","K2X 7H4");
+INSERT INTO `user` (`lastname`,`firstname`,`phone_number`,`email`,`subscription_type`)
+VALUES
+  ("Bernard","Mallory","03 78 78 56 01","vitae.aliquam.eros@protonmail.ca","C8W 4H6"),
+  ("Lloyd","Kerry","08 79 39 78 15","eget.magna@google.com","Y8P 5H7"),
+  ("Rocha","Tallulah","02 63 26 76 38","eu.turpis@google.couk","H1V 8P7"),
+  ("Blackburn","Ryan","05 98 68 78 91","per.inceptos@protonmail.org","Z5Q 0X6"),
+  ("Turner","Anthony","03 49 61 78 11","ipsum.cursus@outlook.couk","N4Q 1H7"),
+  ("Hale","Selma","05 71 50 47 02","aliquam.nisl.nulla@hotmail.com","O7V 7M6"),
+  ("Olson","Chaney","08 56 54 40 83","eu.odio@aol.couk","B5C 2Y6"),
+  ("Lowery","Rhoda","07 19 03 72 06","amet@outlook.net","J1K 8W1"),
+  ("Hoffman","Hop","04 41 32 46 67","nunc.sed@protonmail.org","P4I 5S8"),
+  ("Malone","Anastasia","08 30 51 20 16","neque.non@outlook.couk","Q3Y 9Q1");
+INSERT INTO `user` (`lastname`,`firstname`,`phone_number`,`email`,`subscription_type`)
+VALUES
+  ("Gilbert","Claire","03 56 01 45 30","vel@outlook.org","T5R 0S2"),
+  ("Delaney","Dieter","08 86 15 56 60","parturient@hotmail.edu","Y3T 3G1"),
+  ("Carver","Yvonne","08 20 42 67 63","metus.sit.amet@outlook.ca","B2C 7J4"),
+  ("Murray","Kirestin","07 86 90 31 89","purus.nullam.scelerisque@hotmail.net","K2T 1X3"),
+  ("Steele","Alexis","07 63 61 12 94","lorem@yahoo.org","U6H 8M1"),
+  ("Wilson","Daniel","01 88 56 11 17","orci.adipiscing@outlook.ca","G4F 8D3"),
+  ("Nguyen","Patricia","06 89 38 61 49","purus.ac.tellus@outlook.couk","D0H 6H8"),
+  ("Raymond","Audra","08 63 03 23 48","duis@google.org","T1K 0V6"),
+  ("Reynolds","Wyoming","08 25 85 23 36","odio@icloud.com","R4Q 1A4"),
+  ("Baldwin","Marsden","04 81 70 18 54","quisque.porttitor.eros@hotmail.com","T3X 4D3");
